d348a1faccd56ee4d678bf4382589b662997c850
Samrat Man Singh <samratmansingh@gmail.com>
2012-12-18 19:21:32 +0545

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject jopbox "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojars.samrat/clj-oauth "1.4.0-SNAPSHOT"]
+                 ;[oauth-clj "0.1.0"]
+                 ])
---
+++ src/jopbox/client.clj
@@ -0,0 +1,85 @@
+(ns jopbox.client
+  (:require [oauth.client :as oauth]
+            [clj-http.client :as http]))
+
+;; Authorization & Authentication ---------------------------------------------
+(defn make-consumer
+  "Return Dropbox OAuth consumer."
+  [app-key app-secret]
+  (oauth/make-consumer app-key
+                       app-secret
+                       "https://api.dropbox.com/1/oauth/request_token"
+                       "https://api.dropbox.com/1/oauth/access_token"
+                       "https://www.dropbox.com/1/oauth/authorize"
+                       :hmac-sha1))
+
+(defn fetch-request-token
+  "Return a request token that user may authorize."
+  ([consumer callback-uri]
+     (oauth/request-token consumer callback-uri))
+  ([consumer]
+     (fetch-request-token consumer nil)))
+
+(defn authorization-url
+  "Return authorization endpoint."
+  [consumer request-token]
+  (oauth/user-approval-uri consumer
+                           (:oauth_token request-token)))
+
+(defn fetch-access-token-response
+  "Return a map with :oauth_token and :oauth_token_secret."
+  [consumer request-token]
+  (oauth/access-token consumer
+                      request-token))
+
+;; API calls
+
+(defn make-credentials
+  [consumer access-token-response method url body]
+  (oauth/credentials consumer
+                     (:oauth_token access-token-response)
+                     (:oauth_token_secret access-token-response)
+                     method
+                     url
+                     body))
+
+(defn account-info
+  [consumer access-token-response]
+  (http/get "https://api.dropbox.com/1/account/info"
+            {:query-params (make-credentials consumer
+                                             access-token-response
+                                             :GET
+                                             "https://api.dropbox.com/1/account/info"
+                                             nil)}))
+
+(defn delta
+  ([consumer access-token-response cursor]
+     (http/post "https://api.dropbox.com/1/delta"
+            {:query-params (make-credentials consumer
+                                             access-token-response
+                                             :POST
+                                             "https://api.dropbox.com/1/delta"
+                                             nil)}))
+  ([consumer access-token-response]
+     (delta consumer access-token-response nil)))
+
+(defn upload-file
+  "Upload file to Dropbox using PUT.
+     root can be either `dropbox` or `sandbox`"
+  [consumer access-token-response root remote-path local-path]
+  (http/post (str "https://api-content.dropbox.com/1/files_put/" root "/" remote-path)
+            {:query-params (make-credentials consumer
+                                             access-token-response
+                                             :POST
+                                             "https://api-content.dropbox.com/1/files_put/"
+                                             (clojure.java.io/file local-path))}))
+
+(defn metadata
+  "Retrieve file and folder metadata."
+  [consumer access-token-response root path]
+  (http/get (str "https://api.dropbox.com/1/metadata/" root "/" path)
+            {:query-params (make-credentials consumer
+                                             access-token-response
+                                             :GET
+                                             "https://api.dropbox.com/1/metadata/"
+                                             nil)}))
