b8be1b3fa657e2ef9581fee1378facc81349e10f
Samrat Man Singh <samratmansingh@gmail.com>
2013-01-02 10:36:49 +0545

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject jopbox "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Clojure Library to work with Dropbox API"
+  :url "http://github.com/samrat/jopbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojars.samrat/clj-oauth "1.4.0-SNAPSHOT"]
+                 [clj-oauth "1.4.0"]
                  [cheshire "5.0.1"]
                  ])
