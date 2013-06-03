56470474ab03f642a4e0052ed67b7851cec2092d
Samrat Man Singh <samratmansingh@gmail.com>
2013-06-03 21:56:56 +0545

    Bump clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/samrat/jopbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-oauth "1.4.0"]
                  [cheshire "5.0.1"]
                  ])
