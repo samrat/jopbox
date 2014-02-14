296372b3029d67f71e354d0f3d147bf799ec06e4
Samrat Man Singh <samrat@helpshift.com>
2014-02-14 18:15:52 +0530

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jopbox "0.2.0"
+(defproject jopbox "0.2.1"
   :description "A Clojure Library to work with Dropbox API"
   :url "http://github.com/samrat/jopbox"
   :license {:name "Eclipse Public License"
